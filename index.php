1bb0c2178969d94e2303cd8e31ebf4082d40048e
Jack Biggin <me@jackbiggin.me>
2018-11-11 03:33:32 -0500

    Boilerplate & header

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Jack Biggin</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.5.0/css/foundation.min.css" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/css/app.css" />
+  </head>
+  <body>
+    <header>
+        <div class="grid-x grid-margin-x">
+            <div class="cell">
+                <h1>Jack Biggin</h1>
+            </div>
+        </div>
+    </header>
+    <div class="top-bar">
+        <div class="top-bar-center">
+            <ul class="dropdown menu align-center" data-dropdown-menu>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">About Me</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.5.0/js/foundation.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/what-input/5.1.3/what-input.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+
+  </body>
+</html>
