06ac6c5796e17cb0c784c760bfb632c7e4120dba
Jack Biggin <me@jackbiggin.me>
2018-11-11 21:27:25 -0500

    About page text

---
+++ assets/pages/about.php
@@ -0,0 +1,30 @@
+<div class="grid-container">
+    <div class="grid-x grid-padding-x grid-padding-y">
+        <div class="cell small-12 medium-3">
+            <div class="about-sidebar">
+                <img src="./assets/img/profile.jpg" />
+                <a href="https://linkedin.com/in/JackBiggin" class="linkedin"><i class="fab fa-linkedin"></i> LinkedIn</a>
+                <a href="https://github.com/JackBiggin" class="github"><i class="fab fa-github"></i> GitHub</a>
+                <a href="https://devpost.com/JackBiggin" class="devpost"><i class="fab fa-dev"></i> Devpost</a>
+            </div>
+        </div>
+        <div class="cell small-12 medium-9">
+            <p class="handwritten">Hi, I'm Jack!</p>
+            <p>Right now I'm a <a href="https://ahc.leeds.ac.uk/courses/g856/digital-media-ba">Digital Media student</a> at the <a href="https://leeds.ac.uk">University of Leeds</a>, on my exchange year at <a href="http://uwo.ca">Western University</a> in Canada.</p>
+            <p>When I'm not flying around North America, I'm <a href="http://thisislsr.com">Leeds Student Radio</a>'s Head of Software. I've done all sorts of cool stuff there - from building radio studios to developing in-house software and designing their rather nice website. I also lend a hand to Leeds Student Television whenever needed, as their tech has a tendency to break.</p>
+            <p>When I am flying around North America, I'm often attending <a href="https://mlh.io/faq#what-is-a-hackathon">hackathons</a>. Well, that, or I've gone on holiday to NYC for the <span style="text-decoration: line-through">4th</span> 5th time.</p>
+            <p class="handwritten">And now to shamelessly list some awards I've won...</p>
+            <ul>
+                <li>❤️ Hero of the Moment at the <a href="https://www.studentradio.org.uk/2018/04/the-i-love-student-radio-awards-2018-winners/">I Love Student Radio Awards 2018</a></li>
+                <li>⭐ Most Committed Committee Member (Shortlisted) at the <a href="https://www.studentradio.org.uk/conference/i-love-student-radio-awards/">I Love Student Radio Awards 2018</a></li>
+                <li>🌟 Outstanding Committee Member from Leeds Student Radio (2018)</li>
+                <li>📺 Unsung Hero Award (Nominated) from Leeds Student Television (2017)</li>
+                <li>👑 Special Recognition for a Member Award from Leeds Student Radio (2016)</li>
+            </ul>
+
+            <p>I also was a major factor in Leeds Student Radio winning 🥉 Best Technical Achievement (Bronze) and 🥉 Best Station (Bronze) at the <a href="https://www.studentradio.org.uk/2018/11/the-winners-of-the-student-radio-awards-2018-supported-by-bbc-radio-1-and-global/">Student Radio Awards 2018</a>, as well as winning 🥇 Most Improved Station at the <a href="https://www.studentradio.org.uk/2018/04/the-i-love-student-radio-awards-2018-winners/">I Love Student Radio Awards 2018</a>.</p>
+            <p>My hackathon entries haven't done too badly either, with <a href="https://devpost.com/software/ride-fyi">Ride.fyi</a>, a webapp to find the cheapest way to get from A to B, winning five categories at <a href="https://vandyhacks.org">VandyHacks 5</a> including 🗺️ Best Geospatial Application and <a href="https://devpost.com/software/mypiggybank-k6z97n">MyPiggybank</a>, a proof of concept showing how bank transaction data could be further leveraged winning four categories* at <a href="http://grizzhacks.com">GrizzHacks 3</a> including 💻 Best Web Application</a>.
+            <p style="font-size:0.6em">* annoyingly only two of them show up on Devpost, but it also won <i>Best use of Google Cloud Platform</i> and <i>Best use of HERE.com</i>.</p>
+        </div>
+    </div>
+</div>
