4a097e337938dd9ff0dba138f7d0d9b8feaebceb
Jack Biggin <me@jackbiggin.me>
2018-11-11 19:59:58 -0500

    Added Font Awesome

--- index.php
+++ index.php
@@ -33,6 +33,7 @@
     <title><?php echo $prettyPageName; ?> - Jack Biggin</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.5.0/css/foundation.min.css" />
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/css/app.css" />
   </head>
   <body>
