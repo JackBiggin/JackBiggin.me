bae304078f84b1dbca1a4ebdaf796ea0db5e31e2
Jack Biggin <me@jackbiggin.me>
2018-11-11 23:07:21 -0500

    Unfinished placeholder for contact page

--- assets/pages/contact.php
+++ assets/pages/contact.php
@@ -1,4 +1,8 @@
 <div class="grid-container">
     <div class="grid-x grid-padding-x grid-padding-y">
+        <div class="cell small-12">
+            <p class="handwritten">Get in touch!</p>
+            <p>Whether you'd like to hire me, commission a freelance project or even just to ask a question, feel free to send me an email using the form below. If you're old fashioned, you can also send an email to <a href="mailto:jack@jackbiggin.me">jack@jackbiggin.me</a>. If you're the opposite of old fashioned, feel free to <a href="https://linkedin.com/in/JackBiggin">add me on LinkedIn</a> or <a href="https://twitter.com/JackBiggin">send me a Tweet</a>.</p>
+        </div>
     </div>
 </div>
