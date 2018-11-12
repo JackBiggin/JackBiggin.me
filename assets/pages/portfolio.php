944ab11f5aca26b21d6798a3b18d0a705de2b879
Jack Biggin <me@jackbiggin.me>
2018-11-12 02:24:35 -0500

    Testimonial Slider (TODO: move to JSON/add rest)

--- assets/pages/portfolio.php
+++ assets/pages/portfolio.php
@@ -1,65 +1,6 @@
 <div class="grid-container">
+    <h2 class="handwritten">Here's some cool stuff I've made</h2>
     <div class="grid-x grid-padding-x grid-padding-y">
-        <!-- <div class="cell small-12 medium-6 large-4">
-            <div class="portfolio-item">
-                <div class="portfolio-item-text">
-                    <div>
-                        <div>Example Text</div>
-                        <button type="button" class="button">Learn More</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="cell small-12 medium-6 large-4">
-            <div class="portfolio-item">
-                <div class="portfolio-item-text">
-                    <div>
-                        <div>Example Text</div>
-                        <button type="button" class="button">Learn More</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="cell small-12 medium-6 large-4">
-            <div class="portfolio-item">
-                <div class="portfolio-item-text">
-                    <div>
-                        <div>Example Text</div>
-                        <button type="button" class="button">Learn More</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="cell small-12 medium-6 large-4">
-            <div class="portfolio-item">
-                <div class="portfolio-item-text">
-                    <div>
-                        <div>Example Text</div>
-                        <button type="button" class="button">Learn More</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="cell small-12 medium-6 large-4">
-            <div class="portfolio-item">
-                <div class="portfolio-item-text">
-                    <div>
-                        <div>Example Text</div>
-                        <button type="button" class="button">Learn More</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="cell small-12 medium-6 large-4">
-            <div class="portfolio-item">
-                <div class="portfolio-item-text">
-                    <div>
-                        <div>Example Text</div>
-                        <button type="button" class="button">Learn More</button>
-                    </div>
-                </div>
-            </div>
-        </div> -->
         <?php
             $portfolioItems = file_get_contents("./assets/projects.json");
             $portfolioItems = json_decode($portfolioItems, true);
@@ -77,6 +18,36 @@
                     </div>';
             }
         ?>
+    </div>
+    
+    <h2 class="handwritten">And here's some nice things people have said about me</h2>
+    <div class="grid-x grid-padding-x grid-padding-y">
+        <div class="cell small-0 medium-2"></div>
+        <div class="cell small-12 medium-8">
+            <div class="orbit testimonial-slider-container" role="region" aria-label="testimonial-slider" data-orbit>
+                <ul class="orbit-container">
+                    <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
+                    <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
+
+                    <li class="is-active orbit-slide testimonial">
+                        <img src="https://media.licdn.com/dms/image/C4D03AQGaVSj58p1KaA/profile-displayphoto-shrink_800_800/0?e=1547683200&v=beta&t=iDw32lJv9yrIJohZPoVOUV3KGTwnyT98J7ITSXWcR1s" />
+                        <div class="name">Patrick Carter</div>
+                        <div class="title">LSR Station Manager 2018/19</div>
+                        <p>Jack is a dedicated and intelligent member of the team who works harder than pretty much anyone I know. He thinks outside the box and finds solutions to problems that nobody else could. He is clearly an outstanding member of any team.</p>
+                        <div class="verify"><a href="https://linkedin.com/in/JackBiggin"><i class="fas fa-check-circle"></i> Verified on LinkedIn</a></div>
+                    </li>
+
+                    <li class="orbit-slide testimonial">
+                        <img src="https://media.licdn.com/dms/image/C4D03AQECK9oUeXSr7g/profile-displayphoto-shrink_800_800/0?e=1547683200&v=beta&t=OgQE1GE9D5vZdAUr7QvVNlmaVKi4PLMyfZT-Hm3hBaw" />
+                        <div class="name">Emma Rice</div>
+                        <div class="title">LSR Station Manager 2016/17</div>
+                        <p>Jack is a key team member at Leeds Student Radio. His technical ability is imperative in keeping our online radio station working. [...] I would therefore highly recommend Jack for any position. His intellect and character would be of benefit to any employer. </p>
+                        <div class="verify"><a href="https://linkedin.com/in/JackBiggin"><i class="fas fa-check-circle"></i> Verified on LinkedIn</a></div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="cell small-0 medium-2"></div>
     </div>
 </div>
 
--- index.php
+++ index.php
@@ -32,8 +32,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php echo $prettyPageName; ?> - Jack Biggin</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.5.0/css/foundation.min.css" />
-    <link href="https://fonts.googleapis.com/css?family=Caveat+Brush|Open+Sans:400,600,700,800" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Caveat+Brush|Open+Sans:400,600,700,800" rel="stylesheet" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/motion-ui/2.0.3/motion-ui.min.css" />
     <link rel="stylesheet" href="./assets/css/app.css" />
   </head>
   <body>
@@ -62,6 +63,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.5.0/js/foundation.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/what-input/5.1.3/what-input.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/motion-ui/2.0.3/motion-ui.min.js"></script>
     <script>
       $(document).foundation();
     </script>
